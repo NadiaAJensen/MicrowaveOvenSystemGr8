d90289d996b88d536ec93092d87fe03702aff106
NadiaAJensen <78858892+NadiaAJensen@users.noreply.github.com>
2022-04-07 14:31:53 +0200

    Namespace ændret i to klasser i boundary og program kode tilføjet

--- MicrowaveOvenSystemGr8/Program.cs
+++ MicrowaveOvenSystemGr8/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MicrowaveOvenLibrary.Boundary;
+using MicrowaveOvenLibrary.Interfaces;
 
 namespace MicrowaveOvenSystemGr8
 {
@@ -6,7 +8,43 @@ namespace MicrowaveOvenSystemGr8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Button startCancelButton = new Button();
+            Button powerButton = new Button();
+            Button timeButton = new Button();
+
+            Door door = new Door();
+
+            Output output = new Output();
+
+            Display display = new Display(output);
+
+            PowerTube powerTube = new PowerTube(output);
+
+            Light light = new Light(output);
+
+            Microwave.Classes.Boundary.Timer timer = new Timer();
+
+            CookController cooker = new CookController(timer, display, powerTube);
+
+            UserInterface ui = new UserInterface(powerButton, timeButton, startCancelButton, door, display, light, cooker);
+
+            // Finish the double association
+            cooker.UI = ui;
+
+            // Simulate a simple sequence
+
+            powerButton.Press();
+
+            timeButton.Press();
+
+            startCancelButton.Press();
+
+            // The simple sequence should now run
+
+            System.Console.WriteLine("When you press enter, the program will stop");
+            // Wait for input
+
+            System.Console.ReadLine();
         }
     }
 }
